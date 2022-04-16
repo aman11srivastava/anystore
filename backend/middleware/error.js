b7d876a2c11d12d4ca4067fce91a5542824cfc4d
Aman Srivastava <aman.srivastava@thoughtwizards.com>
2022-04-16 15:22:45 +0530

    Some error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json web token is invalid, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong jwt expire error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json web token has expired, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
